04b7daa601a6ef6f72845794bdc268968c9b4382
gustsirt <gustavo.sirtori@gmail.com>
2023-06-26 23:27:10 -0300

    ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,11 @@
+class Vehiculo:
+  color = "Blanco"
+  ruedas = None
+  puerta = None
+
+class Coche(Vehiculo):
+  Velocidad = None
+  Cilindrada = None
+
+fordK = Coche()
+print("El vehiculo es de color", fordK.color)
